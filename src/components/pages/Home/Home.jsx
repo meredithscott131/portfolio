995d315043d5cb3d0219026eb668538ea4b91b17
meredithscott131 <scott.mer@northeastern.edu>
2024-05-03 21:07:32 -0400

    refined spacing

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import videoBg from '../../../assets/flyThrough.mp4'
+import videoBg from '../../../../assets/flyThrough.mp4'
 import styles from "./Home.module.css";
 
 export const Home = () => {
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import "./index.css";
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
+import "@fontsource/montserrat"
+import "@fontsource/rubik"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
