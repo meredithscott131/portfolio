407cce7d7fb5d952b073833679b54bafe68a0bb5
meredithscott131 <scott.mer@northeastern.edu>
2024-05-10 18:11:28 -0400

    started media page

--- src/components/pages/Animation/Animation.jsx
+++ src/components/pages/Animation/Animation.jsx
@@ -1,6 +1,34 @@
 import React from "react";
 import styles from "./Animation.module.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "../../../vars.css";
+
+import Carousel from 'react-bootstrap/Carousel';
 
 export const Animation = () => {
-  return <h1>Animation</h1>;
+  return <section className={styles.container}>
+      <h2 className={styles.title}>
+        Art and Animation
+      </h2>
+      <div className={styles.contentOne}>
+        <div class="ratio ratio-16x9">
+          <iframe src="https://www.youtube.com/embed/_e9yMqmXWo0?si=xWUEr8Zti0Vwwn-s" title="Chipi chipi chapa chapa cat" allowFullScreen></iframe>
+        </div>
+        <div class={styles.descOne}>
+          <h3 className={styles.subtitle}>
+            The Trespassers
+          </h3>
+          <p className={styles.bodyText}>
+              <span className={styles.maya}>Maya</span>
+              <span>, </span>
+              <span className={styles.sp}>Substance Painter</span>
+              <span>, </span>
+              <span className={styles.rigging}>Rigging</span>
+              <span>, </span>
+              <span className={styles.animation}>Animation</span>
+          </p>
+          <p className={styles.bodyText}>After a boy sneaks into the astronomy exhibit after hours, he realizes he might not be alone.</p>
+        </div>
+      </div>
+  </section>
 };
