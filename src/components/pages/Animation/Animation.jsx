bb991152d9a01ad10f56778e22efa13019baa745
meredithscott131 <scott.mer@northeastern.edu>
2024-05-10 22:45:57 -0400

    added content

--- src/components/pages/Animation/Animation.jsx
+++ src/components/pages/Animation/Animation.jsx
@@ -10,9 +10,9 @@ export const Animation = () => {
       <h2 className={styles.title}>
         Art and Animation
       </h2>
-      <div className={styles.contentOne}>
-      <div class="ratio ratio-16x9">
-          <iframe src="https://www.youtube.com/embed/_e9yMqmXWo0?si=xWUEr8Zti0Vwwn-s" title="Chipi chipi chapa chapa cat" allowFullScreen></iframe>
+      <div className={styles.content}>
+        <div class="ratio ratio-16x9">
+          <iframe src="https://www.youtube.com/embed/fOIAYm4iPOQ?si=q0oV733GdA-etP1F" title="Chipi chipi chapa chapa cat" allowFullScreen></iframe>
         </div>
         <div class={styles.descOne}>
           <h3 className={styles.subtitle}>
@@ -28,6 +28,90 @@ export const Animation = () => {
               <span className={styles.animation}>Animation</span>
           </p>
           <p className={styles.bodyText}>After a boy sneaks into the astronomy exhibit after hours, he realizes he might not be alone.</p>
+        </div>
+      </div>
+      <div className={styles.slideshowOne}>
+      <Carousel interval={null} controls={true}> 
+        <Carousel.Item> 
+          <video 
+            controls
+            autoPlay
+            loop
+            className="d-block w-100"
+            src="assets\animation\flyThrough.mp4"
+            alt="Image One"
+          /> 
+          <Carousel.Caption> 
+            <h3>Fly Through</h3>
+          </Carousel.Caption> 
+        </Carousel.Item>
+        <Carousel.Item> 
+          <video 
+            className="d-block w-100"
+            controls
+            autoPlay
+            loop
+            src="assets\animation\MeredithScott_Turnaround.mp4"
+            alt="Image One"
+          /> 
+          <Carousel.Caption> 
+            <h3>Character Turnaround</h3>
+          </Carousel.Caption> 
+        </Carousel.Item> 
+        <Carousel.Item> 
+          <img 
+            className="d-block w-100"
+            src="assets\animation\Still1.png"
+            alt="Image Two"
+          />  
+        </Carousel.Item>
+        <Carousel.Item> 
+          <img 
+            className="d-block w-100"
+            src="assets\animation\Still2.png"
+            alt="Image Two"
+          />  
+        </Carousel.Item> 
+        <Carousel.Item> 
+          <img 
+            className="d-block w-100"
+            src="assets\animation\Still3.png"
+            alt="Image Two"
+          />  
+        </Carousel.Item>
+        <Carousel.Item> 
+          <img 
+            className="d-block w-100"
+            src="assets\animation\Pose1.jpg"
+            alt="Image Two"
+          />  
+        </Carousel.Item>
+        <Carousel.Item> 
+          <video 
+            className="d-block w-100"
+            controls
+            autoPlay
+            loop
+            src="assets\animation\SHOT8_Test.mp4"
+            alt="Image One"
+          />
+        </Carousel.Item>   
+      </Carousel>
+      </div>
+      <div className={styles.content}>
+        <div class="ratio ratio-16x9">
+          <iframe src="https://www.youtube.com/embed/ztXT_-JpKUk?si=lTokBch_5W3v_i_i" title="Talent Show" allowFullScreen></iframe>
+        </div>
+        <div class={styles.descOne}>
+          <h3 className={styles.subtitle}>
+            Talent Show
+          </h3>
+          <p className={styles.bodyText}>
+              <span className={styles.maya}>Maya</span>
+              <span>, </span>
+              <span className={styles.animation}>Animation</span>
+          </p>
+          <p className={styles.bodyText}>A duck takes the stage! Does he have what it takes?</p>
         </div>
       </div>
   </section>
