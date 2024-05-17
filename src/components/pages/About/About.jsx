015f94709314b4cf82bdb3f3f6ffe228473cc5b8
meredithscott131 <scott.mer@northeastern.edu>
2024-05-17 13:36:52 -0400

    fixed spacing issue with about page

--- src/components/pages/About/About.jsx
+++ src/components/pages/About/About.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import styles from "./About.module.css";
-import { getImageUrl } from "../../../utils";
 
 export const About = () => {
   return <section className={styles.container}>
     <div className={styles.content}>
-      <h2 className={styles.title}>Hi, I'm Meredith!</h2>
-      <p className={styles.description}>Cras sit amet tortor eu arcu gravida porta. Fusce et elementum nulla, vel congue ante. Etiam pulvinar enim a feugiat maximus. Donec molestie augue est, quis lacinia tellus rutrum et. Proin luctus convallis tellus non euismod. Aliquam faucibus tempus ligula, eu facilisis risus venenatis sed.</p>
-      <h3 className={styles.subtitle}>Contact</h3>
-      <ul className={styles.socialsList}>
-        <li className={styles.social}>
-          <img src="../assets/linkedinIcon.png" className={styles.socialImage} alt="LinkedIn"></img>
-          <a href="https://www.linkedin.com/in/meredithgscott/" className={styles.socialText} target="_blank">www.linkedin.com/in/meredithgscott/</a>
+      <ul className={styles.contentItems}>
+        <li className={styles.text}>
+          <h2 className={styles.title}>Hi, I'm Meredith!</h2>
+          <p className={styles.description}>Cras sit amet tortor eu arcu gravida porta. Fusce et elementum nulla, vel congue ante. Etiam pulvinar enim a feugiat maximus. Donec molestie augue est, quis lacinia tellus rutrum et. Proin luctus convallis tellus non euismod. Aliquam faucibus tempus ligula, eu facilisis risus venenatis sed.</p>
+          <h3 className={styles.subtitle}>Contact</h3>
+          <ul className={styles.socialsList}>
+            <li className={styles.social}>
+              <img src="../assets/linkedinIcon.png" className={styles.socialImage} alt="LinkedIn"></img>
+              <a href="https://www.linkedin.com/in/meredithgscott/" className={styles.socialText} target="_blank">www.linkedin.com/in/meredithgscott/</a>
+            </li>
+            <li className={styles.social}>
+              <img src="../assets/githubIcon.png" className={styles.socialImage} alt="gitHub"></img>
+              <a href="https://github.com/meredithscott131" className={styles.socialText} target="_blank">github.com/meredithscott131</a>
+            </li>
+            <li className={styles.social}>
+              <img src="../assets/emailIcon.png" className={styles.socialImage} alt="Email"></img>
+              <a href="mailto:meredithscott131@gmail.com" className={styles.socialText} target="_blank">meredithscott131@gmail.com</a>
+            </li>
+            <li className={styles.resume}>
+              <a href="assets\Resume.pdf" className={styles.resume} target="_blank">Resume</a>
+            </li>
+          </ul>
         </li>
-        <li className={styles.social}>
-          <img src="../assets/githubIcon.png" className={styles.socialImage} alt="gitHub"></img>
-          <a href="https://github.com/meredithscott131" className={styles.socialText} target="_blank">github.com/meredithscott131</a>
-        </li>
-        <li className={styles.social}>
-          <img src="../assets/emailIcon.png" className={styles.socialImage} alt="Email"></img>
-          <a href="mailto:meredithscott131@gmail.com" className={styles.socialText} target="_blank">meredithscott131@gmail.com</a>
-        </li>
-        <li className={styles.resume}>
-          <a href="assets\Resume.pdf" className={styles.resume} target="_blank">Resume</a>
+        <li className={styles.pfp}>
+        <img src="../assets/tempCodePreview.png" alt="profile image"></img>
         </li>
       </ul>
     </div>
-    <img src="../assets/tempCodePreview.png" className={styles.pfp} alt="profile image"></img>
   </section>
 };
