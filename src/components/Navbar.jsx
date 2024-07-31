7e783056176fc2d0596439b7d7c5937088de2196
meredithscott131 <scott.mer@northeastern.edu>
2024-07-30 21:56:28 -0400

    Started home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ export const Navbar = () => {
             <NavLink to="/code">Code</NavLink>
           </li>
           <li className={styles.menuItem}>
-            <NavLink to="/animation">3D Art</NavLink>
+            <NavLink to="/animation">3D Animation</NavLink>
           </li>
           <li className={styles.menuItem}>
             <NavLink to="/about">About</NavLink>
--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import styles from "./Home.module.css";
-import {useEffect} from "react";
+import React from 'react';
+import styles from './Home.module.css';
+
 
 export const Home = () => {
-  
-  return <section>
-      <div class={styles.launchPage}>
+  return (
+    <section>
+      <div className={styles.launchPage}>
       </div>
       <div className={styles.content}>
-        <h1 class={styles.title}>Meredith Scott</h1>
-        <p class={styles.text}>Computer science and animation student, passionate about developing creative technology.</p>
+        <h1 className={styles.title}>Meredith Scott</h1>
       </div>
     </section>
+  );
 };
